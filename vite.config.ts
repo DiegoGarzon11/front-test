871a6483710fc485ca783c4ca5cf95ac7367e5e4
Diego Garzon <diegogarzon357@gmail.com>
2026-07-25 20:03:27 -0500

    setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import type { Config } from 'jest';
-
-const config: Config = {
-	preset: 'ts-jest/presets/default-esm',
-	testEnvironment: 'jest-environment-jsdom',
-	extensionsToTreatAsEsm: ['.ts', '.tsx'],
-	moduleNameMapper: {
-		'^(\\.{1,2}/.*)\\.js$': '$1',
-		'\\.(css|less|scss)$': 'identity-obj-proxy',
-	},
-	transform: {
-		'^.+\\.tsx?$': [
-			'ts-jest',
-			{
-				useESM: true,
-				tsconfig: './tsconfig.app.json',
-			},
-		],
-	},
-	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-};
-
-export default config;
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
